97c44f77ea9cf5e68feb275182c019d58d1ed0ea
rithikronald <rithikronald29@gmail.com>
2022-07-17 18:39:52 +0530

    created and deployed smart_contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xb53556B0EfF8B9a814CA5BBa6D2298EA62281522";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-rinkeby.alchemyapi.io/v2/NptKikD1QMWU6c2AFtJER6ffrjpkzxps
+// require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks:{
+    rinkeby:{
+      url:"https://eth-rinkeby.alchemyapi.io/v2/NptKikD1QMWU6c2AFtJER6ffrjpkzxps",
+      accounts:['5a2dc501e2d5a0ec2f3590d535ad6657043be84ed8680f1fa6089ce3dc1d32d6']
+    }
+  }
 };
